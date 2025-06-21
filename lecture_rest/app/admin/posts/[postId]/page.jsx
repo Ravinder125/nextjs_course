8f5bb3986fa7b055efcf83bad25f8ad60a592e02
Ravinder <ravinder729090@gmail.com>
2025-06-21 15:08:22 +0530

    refactor: remove unused useRouter import and console logs from PostId component

--- lecture_rest/app/admin/posts/[postId]/page.jsx
+++ lecture_rest/app/admin/posts/[postId]/page.jsx
@@ -1,12 +1,9 @@
 "use client"
 
-import { useParams, useRouter } from "next/navigation"
+import { useParams } from "next/navigation"
 
 const PostId = () => {
     const params = useParams();
-    const router = useRouter()
-    console.log(router)
-    console.log(params)
     const id = params.postId
     return (
         <div>PostId : {id}</div>
