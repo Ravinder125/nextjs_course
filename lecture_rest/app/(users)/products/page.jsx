ae36635a59aee15ecc2e9ccf43876efbf95b96f4
Ravinder <ravinder729090@gmail.com>
2025-06-22 18:40:51 +0530

    lecture_18_URL_Data_Client_COMPONENT : useSearchParams and React use api with searchParams in Client Component

--- lecture_rest/app/(users)/products/page.jsx
+++ lecture_rest/app/(users)/products/page.jsx
@@ -1,8 +1,39 @@
 
+// searchParams (Server)
+// const Products = async ({ searchParams }) => {
+//     const { category = "all", sort, page = 1 } = await searchParams;
 
-const Products = async ({ searchParams }) => {
-    const { category = "all", sort, page = 1 } = await searchParams;
+//     return (
+//         <>
+//             <div>category: {category}</div>
+//             <div>sort: {sort}</div>
+//             <div>page: {page}</div>
+//         </>
+//     )
+// }
 
+// export default Products
+
+
+
+// useSearchParams (Client)
+"use client"
+
+import { useSearchParams } from "next/navigation"
+import { use } from "react"
+
+const Products = ({ searchParams }) => {
+    // const searchParams = useSearchParams();
+    // const categories = searchParams.getAll("category")
+    // console.log(categories)
+    // const page = searchParams.get("page")
+    // const sort = searchParams.get("sort")
+
+    // React.use with searchParams
+    const searchParam = use(searchParams)
+    const category = searchParam.category
+    const page = searchParam.page
+    const sort = searchParam.sort
     return (
         <>
             <div>category: {category}</div>
